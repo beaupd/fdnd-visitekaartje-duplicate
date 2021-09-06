01e06e7820326ff2f7c73c24df1850695cd937ca
beaupd <beau@patrickdekker.nl>
2021-09-06 14:25:42 +0200

    changes to body, css and javascript. basic shaping

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,16 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
 
-function expand() {
-    document.body.classList.toggle('expand')
+button = document.querySelector('header button');
+buttons = document.querySelectorAll('header button');
+navItemLength = 180/buttons.length;
+
+buttonHeight = button.offsetHeight;
+buttonTextHeight = parseInt(window.getComputedStyle(button).fontSize, 10);
+console.log(buttonHeight);
+
+main = document.querySelector('main');
+
+console.log(navItemLength);
+
+for (i=0;i<buttons.length; i++){
+    
 }
