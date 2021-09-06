2e67e17eccd242402b6179e6b9823c948c69d0bf
beaupd <beau@patrickdekker.nl>
2021-09-06 21:34:54 +0200

    circular navigation, css en begin layout cards

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,20 @@
 button = document.querySelector('header button');
 buttons = document.querySelectorAll('header button');
 navItemLength = 180/buttons.length;
-
 buttonHeight = button.offsetHeight;
+
 buttonTextHeight = parseInt(window.getComputedStyle(button).fontSize, 10);
-console.log(buttonHeight);
 
 main = document.querySelector('main');
 
-console.log(navItemLength);
-
-for (i=0;i<buttons.length; i++){
-    
+function initButtons() {
+    for (i=0;i<buttons.length; i++){
+        buttonHeight = button.offsetHeight;
+        let b = buttons[i];
+        let degreeOffset = navItemLength*i;
+        b.style.marginLeft = "-"+(b.offsetWidth/2)+"px";
+        b.style.transform = "rotate("+ ((90+navItemLength/2)+degreeOffset) +"deg) translateY("+((main.offsetWidth/2)-buttonHeight)+"px) rotate(180deg)";
+    }
 }
+
+(()=>{initButtons()})()
